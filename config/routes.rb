85189edff50942a6a520711711917c67c5438046
3333ren <kde.nd0611@gmail.com>
2021-04-29 14:10:47 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=======
   devise_for :users
   root to: "alarms#index"
   resources :alarms, only: [:index, :new, :create, :destroy]
   get "alarms/start", to: "alarms#start"
   get "alarms/end", to: "alarms#end"
   resources :memos, only: [:index, :create]
->>>>>>> Stashed changes
 end
