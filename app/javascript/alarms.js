6c36517119b09b61c423088e857ad7de0a975e9c
3333ren <kde.nd0611@gmail.com>
2021-05-06 23:22:39 +0900

    アラーム音声導入

--- app/javascript/alarms.js
+++ app/javascript/alarms.js
@@ -17,6 +17,14 @@ const timeCount = () => {
       let seconds = (alarmTime % 60).toString().padStart(2, "0");
       timeDisplay.innerHTML = `${minutes}:${seconds}`
       if ( alarmTime == 0 ) {
+        let speech = window.speechSynthesis
+        let endMsg = new SpeechSynthesisUtterance;
+        endMsg.volume = 1;
+        endMsg.rate = 1;
+        endMsg.pitch = 1;
+        endMsg.text = "Hello Hello Hello";
+        endMsg.lang = "en-US";
+        speech.speak(endMsg);
        
         clearInterval(intervalId);
         if ( goodText.value.length >= 1 || blackText.value.length >= 1 ) {
