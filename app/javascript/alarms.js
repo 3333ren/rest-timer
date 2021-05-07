1e0e36c6ea125f0ddb4e2e37d74bd7e06769c5b3
3333ren <kde.nd0611@gmail.com>
2021-05-07 23:01:45 +0900

    アラーム音声修正

--- app/javascript/alarms.js
+++ app/javascript/alarms.js
@@ -2,37 +2,43 @@ const timeCount = () => {
   let timeDisplay = document.getElementById("time-display");
   let timeList = document.getElementById("time-list");
   let startButton = document.getElementById("start-button");
-  let sound = document.getElementById("sound");
   let goodText = document.getElementById("good-text");
   let blackText = document.getElementById("black-text");
   let memoButton = document.getElementById("memo-button");
   
+  let finish = () => {
+    let speech = window.speechSynthesis
+    let endMsg = new SpeechSynthesisUtterance;
+    endMsg.volume = 1;
+    endMsg.rate = 1;
+    endMsg.pitch = 1;
+    endMsg.text = "Break time is over";
+    endMsg.lang = "en-US";
+    speech.speak(endMsg);
+    endMsg.onend = () => {
+      window.location.href=""
+    }
+  }
+
+  let memoCheck = () => {
+    if ( goodText.value.length >= 1 || blackText.value.length >= 1 ) {
+      memoButton.click();
+    };
+  }
 
   startButton.addEventListener("click", (e) => {
     e.preventDefault();          
     let alarmTime = 10;
-    let intervalId = setInterval(function () {
+    let intervalId = setInterval( () => {
       alarmTime--;
       let minutes = Math.floor(alarmTime / 60).toString().padStart(2, "0");
       let seconds = (alarmTime % 60).toString().padStart(2, "0");
-      timeDisplay.innerHTML = `${minutes}:${seconds}`
+      timeDisplay.innerHTML = `${minutes}:${seconds}`;
+
       if ( alarmTime == 0 ) {
-        let speech = window.speechSynthesis
-        let endMsg = new SpeechSynthesisUtterance;
-        endMsg.volume = 1;
-        endMsg.rate = 1;
-        endMsg.pitch = 1;
-        endMsg.text = "Hello Hello Hello";
-        endMsg.lang = "en-US";
-        speech.speak(endMsg);
-       
         clearInterval(intervalId);
-        if ( goodText.value.length >= 1 || blackText.value.length >= 1 ) {
-          memoButton.click();
-        };
-        window.setTimeout(function () {
-          window.location.href=""
-        }, 5000);
+        finish();
+        memoCheck();
       };
     }, 1000);
   });
